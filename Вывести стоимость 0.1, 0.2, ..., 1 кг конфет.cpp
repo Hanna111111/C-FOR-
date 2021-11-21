9a9731c20fb190116369adcdb79c79019cda2828
HannaAgata <77154229+Hanna111111@users.noreply.github.com>
2021-11-21 11:50:41 +0100

    Update Вывести стоимость 0.1, 0.2, ..., 1 кг конфет.cpp

--- Вывести стоимость 0.1, 0.2, ..., 1 кг конфет.cpp
+++ Вывести стоимость 0.1, 0.2, ..., 1 кг конфет.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 /*
-
 For5◦. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 0.1,
 0.2, ..., 1 кг конфет.
-
 */
+
+int
+main ()
+{
+  float A;
+
+  cout << "Cost of the sweets: " << endl;
+  cin >> A;
+
+  for (float i = 0.1; i < 1.1; i += 0.1){
+      cout << i << " sweets " << i * A << endl;
+      
+  }
+
+  return 0;
+}
